00da85efccef4f9e183bdc8c33c24057ebef8c62
unknown <patticus.tv@gmail.com>
2022-05-03 21:13:55 +1000

    adding server file comments, server npm and concurrently package.json

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+// # IMPORT: ApolloServer class
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth')
 
+// #IMPORT: the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -26,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 
-// Create a new instance of an Apollo server with the GraphQL schema
+// #CREATE: a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -39,6 +41,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
-// Call the async function to start the server
+// #CALL: the async function to start the server
   startApolloServer(typeDefs, resolvers);
  
