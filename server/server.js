f80e0c17e8e022eefbd9d05e3c102331193c1d8e
unknown <patticus.tv@gmail.com>
2022-05-03 21:50:50 +1000

    commented server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-// # IMPORT: ApolloServer class
+// # IMPORT: express package
 const express = require('express');
+// # IMPORT: ApolloServer class
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth')
@@ -9,24 +10,28 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+// #INITIALIZE: app variable to the value of express()
 const app = express();
+// #INITIALIZE: server variable as a new ApolloServer
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+// #Middleware: parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// #IF: we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// #GET Route for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 // #CREATE: a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
