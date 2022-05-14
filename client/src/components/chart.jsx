16c7fdb6f79298dd0aa07b03081c4a78a13e1011
unknown <patticus.tv@gmail.com>
2022-05-14 21:42:14 +1000

    deploy fixes

--- client/src/components/chart.jsx
+++ client/src/components/chart.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components';
 
-export default function App() {
+export default function Chart() {
   console.log("does trading view exist", window.TradingView);
   useEffect(() => {
     new window.TradingView.MediumWidget(
